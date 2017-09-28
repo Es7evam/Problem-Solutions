793a3dfc4b48287648ded9d8e91b75eb588a1f85
Estevam <estevamarantes@gmail.com>
2017-09-28 13:54:07 -0300

    added 463B

---
+++ Ladder1500-1599/463B - Caisa and Pylons.cpp
@@ -0,0 +1,34 @@
+// 30811042	2017-09-28 19:52:53	Estevam	463B - Caisa and Pylons	GNU C++11	Accepted	78 ms	400 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+#define fastcin ios_base::sync_with_stdio(false)
+
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const double PI = acos(-1.0);
+
+int v[100007];
+int main (void) {
+  	fastcin;
+	int n;
+	cin >> n;
+	for(int i=1;i<=n;i++)
+		cin >> v[i];
+		
+	v[0] = 0;
+	int energy = 0, minEnergy = 0;
+	for(int i=1;i<=n;i++){
+		energy += -v[i] +v[i-1];
+		minEnergy = min(energy, minEnergy);
+	}
+	
+	cout << -1 * minEnergy << endl;
+	
+  
+	return 0;
+}
