ad2180653b04dc5aa21f6930bd6b1c1b4f5ea1e7
Es7evam <estevam.arantes@usp.br>
2017-10-19 13:39:13 -0200

    add problem 18

---
+++ Ladder1500-1599/18 - 337A - Puzzles.cpp
@@ -0,0 +1,31 @@
+//31512595	2017-10-19 18:36:54	Estevam	337A - Puzzles	GNU C++11	Accepted	30 ms	2000 KB
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+#define fastcin ios_base::sync_with_stdio(false)
+
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const double PI = acos(-1.0);
+
+int v[57];
+
+int main (void) {
+  	fastcin;
+	int n, m;
+	cin >> n >> m;
+	for(int i=0;i<m;i++)
+		cin >> v[i];
+  
+	int diff = INF;
+	sort(v, v+m);
+	for(int i=0;i+n-1<m;i++){
+		diff = min(diff, v[i+n-1] - v[i]);
+	}
+	cout << diff << endl;
+		
+	return 0;
+}
