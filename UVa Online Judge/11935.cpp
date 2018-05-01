b81bd8c40244983be451e6c0c99149651d9507b0
Es7evam <estevam.arantes@usp.br>
2018-05-01 13:36:18 -0300

    Add problem p11935

--- UVa Online Judge/11935.cpp
+++ UVa Online Judge/11935.cpp
@@ -9,6 +9,8 @@
 #include <bitset>
 #include <cstring>
 #include <math.h>
+#include <string>
+#include <iomanip> // setprecision
 
 using namespace std;
 #define pb push_back
@@ -17,77 +19,44 @@ using namespace std;
 #define se second
 typedef long long ll;
 
-int n;
-double k;
-vector <pair<int, string> v;
+#define EPS 1e-6
+#define maxN 110
 
-bool checker(int tam){
-	int cur = tam;
-	int count = 0;
-	for(int i=0;i<=n;i++){
-		if(cur - a[i] < 0){
-			count++;
-			cur = tam;
-			i--;
-		}else{
-			cur -= a[i];
-		}
-		if(count > k){
-			return false;
-		}
+int main(){
+	string event, input;
+	while (getline(cin, input)){
+		//cout << input << endl;
+		if(input == "0 Fuel consumption 0")
+			break;
+
+		stringstream ss(input);
+		int x = 0, y = 0, leak = 0, fph = 0;	//fuel per hundred
+		double res = 0.0, cur = 0.0;
+		string trash;
+		do {
+			stringstream ss(input);
+			ss >> y >> event;
+			//cout << event << ": " << y << endl;
+			cur += (y - x) * fph / 100.0;
+			cur += (y - x) * leak;
+			res = max(res, cur);
+			if (event[0] == 'G' && event[1] == 'o')
+				break;		//goal
+			else if (event[0] == 'L')
+				leak++ ;		//leak
+			else if (event[0] == 'M')
+				leak = 0;		//mechaninc
+			else if (event[0] == 'G')
+				cur = 0.0;	//gas station
+			else
+				ss >> trash >> fph;
+			//sscanf(input, "%d Fuel consumption %d", &y, &fph);
+			x = y;
+		} while (getline(cin, input));
+
+		if(res == 0)
+			break;
+		cout << fixed << setprecision(3) << res << endl;
 	}
-	return true;
-}
-
-ll minansw = 0x3f3f3f3f;
-int run = 0;
-
-void go(ll l, ll r){
-	ll mid = (l+r)/2;
-	if(run > 10000)
-		return;
-	run++;
-	if(checker(mid)){
-		minansw = min(minansw, mid);
-		go(l, mid);
-	}else{
-		go(mid+1, r);
-	}
-}
-
-int main(void){
-	string str1, str2;
-	while(true){
-		cin >> n >> str1 >> str2 >> k;
-		v.clear();
-		if(n == 0 && k == 0)
-			return 0;
-		int nextn;
-		string nextstr1;
-		while(true){
-			cin >> nextn >> nextstr1;
-			if(str1 == "Fuel")
-				v.pb(mk(nextn - n, k/100));
-			if(str1 == "Leak")
-				v.pb(mk(nextn - n, 1.0);
-				
-			str2 = "";
-			if(nextstr1 == "Goal")
-				break;
-			if(nextstr1 == "Gas"){
-				v.pb(mk(-1, 0);
-			}
-			n = nextn;
-			str1 = nextstr1;
-		}
-		minansw = 0x3f3f3f3f, run = 0, sum = 0;
-		memset(a, 0, sizeof(a));
-		for(int i=0;i<=n;i++){
-			cin >> a[i];
-			sum += a[i];
-		}
-		ll l = 0;
-		go(l, sum);
-		cout << minansw << endl;
-	}
+	return 0;
 }
